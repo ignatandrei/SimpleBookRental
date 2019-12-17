2fc7cf4a3594f5940a6a7cbd5034e7b59b98375f
Andrei Ignat <ignatandrei@yahoo.com>
2019-12-17 06:00:27 +0200

    all

--- buildAll.bat
+++ buildAll.bat
@@ -1,3 +1,5 @@
-cd src\WebDashboard
-npm i
-npm run build
+cd src\WebDashboard\book-dashboard
+call npm i
+call npm run build
+del ..\..\BookRental\BookRentalAPI\wwwroot\index.html
+(robocopy build ..\..\BookRental\BookRentalAPI\wwwroot  /MIR /XD) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
