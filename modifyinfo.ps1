05ae16d42447f32113c6ec50dfce6123167f764a
ignatandrei <ignatandrei@yahoo.com>
2019-12-17 07:58:02 +0200

    morfe env

--- modifyinfo.ps1
+++ modifyinfo.ps1
@@ -1,6 +1,7 @@
 $file = ".\src\BookRental\BookRentalAPI\Controllers\InfoController.cs"
 $date = Get-Date -Format "yyyyMMdd:HHmmss"
 $commitText= "andrei"
+Get-ChildItem Env:
 #$commitText = (Get-Item Env:\BUILD_SOURCEVERSIONMESSAGE)
 ((Get-Content -path $file -Raw) -replace '{LatestCommit}',$commitText -replace '{DateCommit}' , $date ) | Set-Content -Path $file
 (Get-Content -path $file -Raw)
