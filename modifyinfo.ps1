47d05b9f216e5f06c00739ab295d2db361d11fa8
ignatandrei <ignatandrei@yahoo.com>
2019-12-17 08:10:54 +0200

    author

--- modifyinfo.ps1
+++ modifyinfo.ps1
@@ -1,7 +1,8 @@
 $file = ".\src\BookRental\BookRentalAPI\Controllers\InfoController.cs"
 $date = Get-Date -Format "yyyyMMdd:HHmmss"
 $commitText= "andrei"
+$author= (Get-Item Env:\BUILD_SOURCEVERSIONAUTHOR)
 Get-ChildItem Env:
 #$commitText = (Get-Item Env:\BUILD_SOURCEVERSIONMESSAGE)
-((Get-Content -path $file -Raw) -replace '{LatestCommit}',$commitText -replace '{DateCommit}' , $date ) | Set-Content -Path $file
+((Get-Content -path $file -Raw) -replace '{LatestCommit}',$commitText -replace '{LastAuthor}',$author -replace '{DateCommit}' , $date ) | Set-Content -Path $file
 (Get-Content -path $file -Raw)
