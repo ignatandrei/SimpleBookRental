bcb5f51e64161ab098ea0847a7f70caf5bf7d6d8
ignatandrei <ignatandrei@yahoo.com>
2019-12-17 08:40:54 +0200

    ref.og

--- modifyinfo.ps1
+++ modifyinfo.ps1
@@ -1,7 +1,7 @@
 $file = ".\src\BookRental\BookRentalAPI\Controllers\InfoController.cs"
 $date = Get-Date -Format "yyyyMMdd:HHmmss"
-$commitText= "andrei"
-$author= (Get-Item Env:\BUILD_SOURCEVERSIONAUTHOR)
+$commitText= & "git.exe" "reflog  -1  --pretty=oneline"
+$author=Get-Variable BUILD_SOURCEVERSIONAUTHOR -valueOnly 
 Get-ChildItem Env:
 #$commitText = (Get-Item Env:\BUILD_SOURCEVERSIONMESSAGE)
 ((Get-Content -path $file -Raw) -replace '{LatestCommit}',$commitText -replace '{LastAuthor}',$author -replace '{DateCommit}' , $date ) | Set-Content -Path $file
