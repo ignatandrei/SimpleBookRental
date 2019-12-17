a88defa452526c4f72c813258b0ee0e388c044f7
Bobby <33196341+shdow@users.noreply.github.com>
2019-12-17 23:50:22 +0200

    Added simple footer and header, modified catalog and card a bit

--- src/WebDashboard/book-dashboard/src/components/bookCard.js
+++ src/WebDashboard/book-dashboard/src/components/bookCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const bookCard = (title, author, description) => {
+const bookCard = (id, author, description) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt='book' src={``} />
-      <h2>{title}</h2>
+      <h2>{id}</h2>
       <h3>{author}</h3>
       <p>{description}</p>
     </div>
