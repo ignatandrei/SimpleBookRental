d8eca6c0b40859cccdda231fd1992441eae3be87
Bobby <33196341+shdow@users.noreply.github.com>
2019-12-18 00:28:21 +0200

    Finally can access data in the api to display on cards in next commit

--- src/WebDashboard/book-dashboard/src/App.js
+++ src/WebDashboard/book-dashboard/src/App.js
@@ -15,20 +15,26 @@ class App extends Component {
     
 
 
-async componentDidMount() {
-  const url = "https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks";
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(this.state.books, 'test');
-  this.setState({ books: data,info:{}})
-  console.log(this.state.books, 'test2');
-  const infoUrl="https://bookrentalapi20191216080922.azurewebsites.net/Info/GetLatestCommit";
-  const responsInfo = await fetch(infoUrl);
-  const dataInfo = await responsInfo.json();
-  // console.log(dataInfo);
-  this.setState({ books: data,info:dataInfo})
-  
-  
+componentDidMount() {
+  // const url = "https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks";
+  // const response = await fetch(url);
+  // const data = await response.json();
+  // console.log(this.state.books, 'test');
+  // this.setState({ books: data,info:{}})
+  // console.log(this.state.books, 'test2');
+  // const infoUrl="https://bookrentalapi20191216080922.azurewebsites.net/Info/GetLatestCommit";
+  // const responsInfo = await fetch(infoUrl);
+  // const dataInfo = await responsInfo.json();
+  // // console.log(dataInfo);
+  // this.setState({ books: data,info:dataInfo})
+  fetch('https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks')
+    .then(response=> response.json())
+    .then(data => this.setState({ books: data }));
+    
+    fetch('https://bookrentalapi20191216080922.azurewebsites.net/Info/GetLatestCommit')
+      .then(response=> response.json())
+      .then(dataInfo => this.setState({ info: dataInfo}));
+    
 }
 
 
@@ -55,7 +61,9 @@ async componentDidMount() {
             </nav>
           </header>
             
-            <h3>{books.length}</h3>
+            <h3>{books[1].title}</h3>
+            <h3>{books[1].authors[0].name}</h3>
+            {console.log(this.state.books, "working api")}
             <ol>
               <h4>{books[1].author}</h4>
             
