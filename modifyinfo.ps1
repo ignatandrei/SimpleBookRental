7916d1b16b791daba6225244ea7bf61e2c808f1e
Andrei Ignat <ignatandrei@yahoo.com>
2019-12-17 10:00:58 +0200

    correct author and commit message

--- modifyinfo.ps1
+++ modifyinfo.ps1
@@ -2,7 +2,8 @@ $file = ".\src\BookRental\BookRentalAPI\Controllers\InfoController.cs"
 $date = Get-Date -Format "yyyyMMdd:HHmmss"
 Get-ChildItem Env:
 
-$author=" test"
-$commitText = $Env:BUILD_SOURCEVERSIONAUTHOR
+$author= $Env:BUILD_SOURCEVERSIONAUTHOR
+
+$commitText = $env:BASH_COMMITMESSAGE
 ((Get-Content -path $file -Raw) -replace '{LatestCommit}',$commitText -replace '{LastAuthor}',$author -replace '{DateCommit}' , $date ) | Set-Content -Path $file
 (Get-Content -path $file -Raw)
