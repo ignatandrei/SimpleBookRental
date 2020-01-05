51567b3d58262744a6be64936e8d40b1f2235cad
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-05 21:55:10 +0200

    Removed dead code, tidyed up some code

--- src/WebDashboard/book-dashboard/src/App.js
+++ src/WebDashboard/book-dashboard/src/App.js
@@ -7,36 +7,6 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     books: [],
-  //     info: null
-  //   }
-  // }
-
-      
-      
-  // const url = "https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks";
-  // const response = await fetch(url);
-  // const data = await response.json();
-  // console.log(this.state.books, 'test');
-  // this.setState({ books: data,info:{}})
-  // console.log(this.state.books, 'test2');
-  // const infoUrl="https://bookrentalapi20191216080922.azurewebsites.net/Info/GetLatestCommit";
-  // const responsInfo = await fetch(infoUrl);
-  // const dataInfo = await responsInfo.json();
-  // // console.log(dataInfo);
-  // this.setState({ books: data,info:dataInfo})
-  // fetch('https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks')
-  //   .then(response=> response.json())
-  //   .then(data => {this.setState({ books: data })});
-    
-    // fetch('https://bookrentalapi20191216080922.azurewebsites.net/Info/GetLatestCommit')
-    //   .then(response=> response.json())
-    //   .then(dataInfo => this.setState({ info: dataInfo}));
-    
-    
   
 
   render() {
