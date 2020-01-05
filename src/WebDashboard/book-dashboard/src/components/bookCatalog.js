7c60222aa0d8927e16bb1e7dfe940ec6c7b707cb
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-05 21:31:32 +0200

    finally working and visible api, cleaned components a bit, happy new year!

--- src/WebDashboard/book-dashboard/src/components/bookCatalog.js
+++ src/WebDashboard/book-dashboard/src/components/bookCatalog.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import bookCard from './bookCard';
+import React, {useState, useEffect} from 'react';
+import BookCard from './BookCard';
+
 
 
 // console.log(this.props.title, "catalog");
-const bookCatalog = () => {
+const BookCatalog = () => {
+
+  const [ books, setBooks ] = useState([]);
+
+    useEffect(() => {
+        const getBooks = async () => {
+            const response = await fetch("https://bookrentalapi20191216080922.azurewebsites.net/Book/GetBooks");
+            const jsonResponse = await response.json();
+            console.log(jsonResponse.books, 'working api');
+            setBooks(jsonResponse.books);
+        };
+
+        getBooks();
+    }, []);
+
   return (
     <div>
       {
-        this.state.books.map((books, i) => {
-          return <bookCard
+        books.map((book, i) => {
+          return <BookCard
           key={i}
           id={books[i].id}
+          author={books[i].authors[0].name}
           title={books[i].title}
           description={books[i].description}
           
           />
-        })    
-        
-        
+          
+        })
       }
+      
     </div>
   );
+  
 }
 
-export default bookCatalog;
+export default BookCatalog;
