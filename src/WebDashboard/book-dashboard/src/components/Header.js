159c66003adafc3cf4944d9118e0dfa7610ffe6f
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-05 23:10:59 +0200

    Centered everything, played a bit with colors, added tags and MomentJS.

--- src/WebDashboard/book-dashboard/src/components/Header.js
+++ src/WebDashboard/book-dashboard/src/components/Header.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='tc'>
-      <header className="bg-black-90 w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-      <nav class="f6 fw6 ttu tracked">
-      <a class="link dim white dib mr3" href="#" title="Home">Simple Book Rental + Logo</a>
-      <a  class="link dim white dib mr3" href="#" title="About">Rent</a>
-      <a class="link dim white dib mr3" href="#" title="Store">Placeholder</a>
-      <a class="link dim white dib" href="#" title="Contact">Placeholder</a>
+      <header className=" w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+      {<img className="" src="https://img.icons8.com/flat_round/80/000000/bookmark-book.png" />}
+      <nav className="f6 fw6 ttu tracked">
+      
+      <a className="link dim white dib mr3" href="" title="Home">
+       Simple Book Rental</a>
+      <a  className="link dim white dib mr3" href="#" title="About">Rent</a>
+      <a className="link dim white dib mr3" href="#" title="Store">Placeholder</a>
+      <a className="link dim white dib" href="#" title="Contact">Placeholder</a>
       </nav>
       </header>
     </div>
--- src/WebDashboard/book-dashboard/src/components/bookCard.js
+++ src/WebDashboard/book-dashboard/src/components/bookCard.js
@@ -5,10 +5,10 @@ const BookCard = ( {id, author, title, description} ) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       {<img style={{width:300, height:300}} alt='book' src={`https://pixabay.com/get/54e3dd4a4850a514f1dc8460825668204022dfe05459734c742c78dc/book-2389229_640.png`} />}
-      <h2>{id}</h2>
-      <h2>{author}</h2>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h2>ID: {id}</h2>
+      <h2>Author:<br />{author}</h2>
+      <h3>Title:<br />{title}</h3>
+      <p>Description:<br /> {description}</p>
     </div>
   );
 }
