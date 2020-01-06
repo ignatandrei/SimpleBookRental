ae23f4e9937615ef37f7e36e4354b47015e5260f
Andrei Ignat <ignatandrei@yahoo.com>
2020-01-07 00:16:30 +0200

    with rented books

---
+++ src/BookRental/BookRentalAPI/Controllers/UserOperations.cs
@@ -0,0 +1,37 @@
+﻿using BookRentalObjects;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookRentalAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class UserOperationsController: Controller
+    {
+        
+        
+        public string GetActualUser()
+        {
+            if(this.User?.Identity?.IsAuthenticated != true)
+            {
+                return "Sample user";
+            }
+            return this.User.Identity.Name;
+        }
+        [HttpPost("{id}")]
+        public bool RentBook(int id)
+        {
+            var rb = new RentBooks();
+            return rb.RentBook(GetActualUser(), id);
+            
+        }
+        public RentedBook[] RentedBooks()
+        {
+            var rb = new RentBooks();
+            return rb.UserBooks(GetActualUser());
+        }
+    }
+}
---
+++ src/BookRental/BookRentalObjects/RentedBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Drawing;
+//using System.Drawing.Imaging;
+
+namespace BookRentalObjects
+{
+    public class RentedBook
+    {
+        public Book Book { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+    public class RentBooks
+    {
+        static Dictionary<string, List<RentedBook>> books = new Dictionary<string, List<RentedBook>>();
+        public bool RentBook(string userName,int bookId)
+        {
+            //TODO: Concurrency
+            if (books.Values.SelectMany(it => it).Select(it => it.Book.ID).Contains(bookId))
+                return false;
+            if (!books.ContainsKey(userName))
+                books.Add(userName, new List<RentedBook>());
+            
+            var lb = new LoadBooks();
+            var b = lb.Books().FirstOrDefault(it => it.ID == bookId);
+            if (b == null)
+                return false;
+            var rb = new RentedBook();
+            rb.Book = b;
+            rb.FromDate = DateTime.UtcNow;
+            rb.ToDate = rb.FromDate.AddDays(14);//TODO: config
+            books[userName].Add(rb);
+            return true;
+        }
+        public RentedBook[] UserBooks(string userName)
+        {
+            if (!books.ContainsKey(userName))
+                books.Add(userName, new List<RentedBook>());
+
+            return books[userName].ToArray();
+        }
+
+    }
+}
