135bcf60c8acd38f68b11aa938d811ce98eb621e
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-06 23:02:28 +0200

    Added routes.

--- src/WebDashboard/book-dashboard/src/App.js
+++ src/WebDashboard/book-dashboard/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import BookCatalog from './components/BookCatalog';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 
@@ -12,9 +13,12 @@ class App extends Component {
   render() {
     return (
       <div>
+      <BrowserRouter>
         <Header />
         <BookCatalog />
         <Footer />
+      </BrowserRouter>
+        
       </div>
      )
     }
--- src/WebDashboard/book-dashboard/src/components/Header.js
+++ src/WebDashboard/book-dashboard/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Menu, Icon } from 'antd';
 import { Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { Search } = Input;
 
@@ -24,13 +25,12 @@ class Header extends React.Component {
       <Menu className="tc" onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
         <Menu.Item key="mail">
           
-          Simple Book Rental
+        <Link to="/">Simple Book Rental</Link>
         </Menu.Item>
         
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            My Rented Books
-          </a>
+        <Menu.Item key="rentedBooks">
+          <Link to="/MyRentedBooks">My Rented Books</Link>
+          
         </Menu.Item>
         <Search
           placeholder="Search Books"
