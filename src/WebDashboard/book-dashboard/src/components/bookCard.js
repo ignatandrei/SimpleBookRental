5d8565a4a65164011e3722ebf1ae0e6225a82506
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-12 17:34:35 +0200

    Added rentbook id to rent button.

--- src/WebDashboard/book-dashboard/src/components/bookCard.js
+++ src/WebDashboard/book-dashboard/src/components/bookCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 
 
-const BookCard = ( {author, title, description, imgId} ) => {
+const BookCard = ( {author, title, description, imgId, rentId } ) => {
   return (
     <div className="tc dib grow br2 ba dark-gray b--black-10 w-20 mv4 mh2 h-25 mw-30">
       {<img className="br2 br--top h-25 w-100" alt='book' src={`https://bookrentalapi20191216080922.azurewebsites.net/Book/GetImage/${imgId}`} />}
@@ -10,7 +10,7 @@ const BookCard = ( {author, title, description, imgId} ) => {
       <h3>Author:<br />{author}</h3>
       <h5 className="mv0">Title:<br />{title}</h5>
       <p className="">Description:<br /> {description}</p>
-      <Button>Rent</Button>
+      <Button><a src="{`https://bookrentalapi20191216080922.azurewebsites.net/UserOperations/RentBook/${rentId}`}">Rent</a></Button>
     
       </div>
   );
