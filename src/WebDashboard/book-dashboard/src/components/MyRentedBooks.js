d22c851406962dcecaca94cdf5ae62e18ae9e3fe
Bobby <33196341+shdow@users.noreply.github.com>
2020-01-12 08:15:33 +0200

    Added img api, displayed rented books api but it's not yet working/commented searchbox for now.

--- src/WebDashboard/book-dashboard/src/components/MyRentedBooks.js
+++ src/WebDashboard/book-dashboard/src/components/MyRentedBooks.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useContext} from 'react';
+import { BookContext } from './BookContext';
+import BookCard from './BookCard';
 
 const MyRentedBooks = () => {
+
+    const { books } = useContext(BookContext);
+    const { rentBooks, setRentedBook } = useState({});
+
+    // useEffect(() => {
+    //     const rentBooks = async () => {
+    //         const response = await fetch("https://bookrentalapi20191216080922.azurewebsites.net/UserOperations/RentBook/");
+    //         const jsonResponse = await response.json();
+    //         console.log(jsonResponse.rent, 'working api bookcontext');
+    //         setRentedBook(jsonResponse.rent);
+    //     };
+    
+    //     rentBooks();
+    // }, []);
+
     return (
         <div className="tc">
+        <div>
             <h1>My Rented Books</h1>
+        </div>
+        <div>
+        Rented books
+        </div>
+        <div>
+        
+
+        {
+            books.map((book,i) => {
+                return <BookCard 
+                    id={"https://bookrentalapi20191216080922.azurewebsites.net/UserOperations/RentedBooks/${book[i].id}"}
+                
+                />
+            })
+        }
+
+        </div>
+            
         </div>
     )
 }
--- src/WebDashboard/book-dashboard/src/components/bookCard.js
+++ src/WebDashboard/book-dashboard/src/components/bookCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Button } from 'antd';
 
 
-const BookCard = ( {id, author, title, description} ) => {
+const BookCard = ( {author, title, description, imgId} ) => {
   return (
     <div className="tc dib grow br2 ba dark-gray b--black-10 w-20 mv4 mh2 h-25 mw-30">
-      {<img className="br2 br--top h-25 w-100" alt='book' src={`https://images-na.ssl-images-amazon.com/images/I/71qWDVkNMRL.jpg`} />}
+      {<img className="br2 br--top h-25 w-100" alt='book' src={`https://bookrentalapi20191216080922.azurewebsites.net/Book/GetImage/${imgId}`} />}
       
       <h3>Author:<br />{author}</h3>
       <h5 className="mv0">Title:<br />{title}</h5>
       <p className="">Description:<br /> {description}</p>
-    </div>
+      <Button>Rent</Button>
+    
+      </div>
   );
 }
   export default BookCard;
