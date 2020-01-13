c555640e8fcad0c5da90a1d0139579a4155de2a1
ignatandrei <ignatandrei@yahoo.com>
2020-01-13 13:24:24 +0200

    rent book inside

--- src/WebDashboard/book-dashboard/src/components/bookCard.js
+++ src/WebDashboard/book-dashboard/src/components/bookCard.js
@@ -5,14 +5,12 @@ import { Button } from 'antd';
 const BookCard = ( {author, title, description, imgId, rentId } ) => {
   return (
     <>
-    <div className="tc dib grow br2 ba dark-gray b--black-10 w-20 mv4 mh2 h-25 mw-30">
+    <div className="tc dib br2 ba dark-gray b--black-10 w-20 mv4 mh2 h-25 mw-30">
       {<img className="br2 br--top h-25 w-100" alt='book' src={`https://bookrentalapi20191216080922.azurewebsites.net/Book/GetImage/${imgId}`} />}
       
       <h3>Author:<br />{author}</h3>
       <h5 className="mv0">Title:<br />{title}</h5>
       <p className="">Description:<br /> {description}</p>
-      <Button><a src="{`https://bookrentalapi20191216080922.azurewebsites.net/UserOperations/RentBook/${rentId}`}">Rent</a></Button>
-      </div>
       <button onClick={async ()=>{
         window.alert("trying to rent" + imgId);
         var url='https://bookrentalapi20191216080922.azurewebsites.net/UserOperations/RentBook/' +imgId;
@@ -35,6 +33,8 @@ const BookCard = ( {author, title, description, imgId, rentId } ) => {
       }}>
       Rent the {title}
           </button>
+      
+      </div>
       </>    
   );
 }
