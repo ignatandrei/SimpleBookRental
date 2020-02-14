00a5b4ed41317c935e43617d1487ca81dcb8e7ba
ignatandrei <ignatandrei@yahoo.com>
2020-02-14 15:13:43 +0200

    added book component

--- src/WebDashboardNG/book-dash/src/app/app.module.ts
+++ src/WebDashboardNG/book-dash/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { BookComponent } from './book/book.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
