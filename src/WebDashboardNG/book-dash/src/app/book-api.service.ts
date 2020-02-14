9ad636d7a03c800b15d6be08b6b0ac3794c63463
ignatandrei <ignatandrei@yahoo.com>
2020-02-14 14:52:01 +0200

    htttp

--- src/WebDashboardNG/book-dash/src/app/book-api.service.ts
+++ src/WebDashboardNG/book-dash/src/app/book-api.service.ts
@@ -12,7 +12,7 @@ export class BookApiService {
   constructor(private http: HttpClient) { }
   getBookApi(): Observable<Books> {
     let url = environment.apiUrl;
-    url =  'http://localhost:5000/'; // modifica in environemnt.ts si sterge linia aceasta
+    
     return this.http.get<Books>(url + 'Book/GetBooks');
   }
 
