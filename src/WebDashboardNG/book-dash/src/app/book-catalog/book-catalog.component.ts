1b37761d7994504df81d6a5ea126aa1e287082ec
Bobby <33196341+shdow@users.noreply.github.com>
2020-02-18 23:12:45 +0200

    Added Catalog component and fixed header design

---
+++ src/WebDashboardNG/book-dash/src/app/book-catalog/book-catalog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-book-catalog',
+  templateUrl: './book-catalog.component.html',
+  styleUrls: ['./book-catalog.component.css']
+})
+export class BookCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
