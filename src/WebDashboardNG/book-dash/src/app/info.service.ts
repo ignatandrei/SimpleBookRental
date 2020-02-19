969b215428b45b4c1e04d9e88316d81729c3d7da
ignatandrei <ignatandrei@yahoo.com>
2020-02-19 15:56:43 +0200

    repare footer

--- src/WebDashboardNG/book-dash/src/app/info.service.ts
+++ src/WebDashboardNG/book-dash/src/app/info.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { Info } from './info';
+import { Infos } from './Infos';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,10 @@ export class InfoService {
 
   constructor(private http: HttpClient) { }
 
-  getLatestInfo(): Observable<Info[]> {
-    let infoUrl = environment.apiUrl;
+  getLatestInfo(): Observable<Infos> {
 
-    return this.http.get<Info[]>(infoUrl + 'Info/GetLatestCommit');
+    const infoUrl = environment.apiUrl;
+
+    return this.http.get<Infos>(infoUrl + 'Info/GetLatestCommit');
   }
 }
