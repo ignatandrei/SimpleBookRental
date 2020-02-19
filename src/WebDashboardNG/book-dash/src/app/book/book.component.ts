28970ffc475505584230d921ca1fc0feb00563e8
ignatandrei <ignatandrei@yahoo.com>
2020-02-19 16:09:53 +0200

    book + author

--- src/WebDashboardNG/book-dash/src/app/book/book.component.ts
+++ src/WebDashboardNG/book-dash/src/app/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BookApiService } from '../book-api.service';
 import { Books } from "../Books";
 import { Book } from '../Book';
+import { Author } from '../Author';
 
 
 @Component({
@@ -11,11 +12,21 @@ import { Book } from '../Book';
 })
 export class BookComponent implements OnInit {
 
-  public books : Book[];
+  @Input() 
+  public book : Book;
 
   
   constructor( private booksService: BookApiService ) {
-    
+    //to be deteled, because it will be injected Pass data from parent to child with input binding
+    this.book = new Book();
+    this.book.title="AAAAA";
+    this.book.id=8;
+    this.book.description = "ASdada";
+    this.book.authors =[];
+    let a=new Author();
+    a.id=99;
+    a.name = "ASDASD";
+    this.book.authors.push(a);
   }
   ngOnInit() {
     this.booksService.getBookApi().subscribe(data =>{ 
