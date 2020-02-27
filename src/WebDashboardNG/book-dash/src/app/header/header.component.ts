660c1ff00acd8d9597f75b7b5f3116292bb11741
Bobby <33196341+shdow@users.noreply.github.com>
2020-02-27 23:30:28 +0200

    added some stuff for search bar

--- src/WebDashboardNG/book-dash/src/app/header/header.component.ts
+++ src/WebDashboardNG/book-dash/src/app/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
+import { Book } from '../Book';
 
 
 @Component({
@@ -8,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Input() 
+  public book : Book;
+  
   constructor() { }
 
   ngOnInit(): void {
+const searchBox = document.getElementById('search-box');
+
+const typeahead = fromEvent(searchBox, 'input').pipe(
+  map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+  filter(text => text.length > 2),
+  debounceTime(10),
+  distinctUntilChanged(),
+  switchMap(() => ajax('/api/endpoint'))
+);
+
+typeahead.subscribe(data => this.book);
   }
 
 }
