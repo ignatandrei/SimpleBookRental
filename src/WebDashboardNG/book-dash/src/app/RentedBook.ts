5d26ce76c8eaaab344cf7cd703f5f20c69bb70cb
Bobby <33196341+shdow@users.noreply.github.com>
2020-02-27 21:34:15 +0200

    removed rented authors and books, cleaned code a bit, displayed dates with momentjs

--- src/WebDashboardNG/book-dash/src/app/RentedBook.ts
+++
@@ -1,8 +0,0 @@
-import { RentedAuthors } from './RentedAuthors';
-
-export class RentedBook {
-    id: number;
-    title: string;
-    description: string;
-    authors: RentedAuthors[];
-}
--- src/WebDashboardNG/book-dash/src/app/rented-book-page/rented-book-page.component.ts
+++ src/WebDashboardNG/book-dash/src/app/rented-book-page/rented-book-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RentedBooks } from '../RentedBooks';
 import { RentedBookApiService } from '../rented-book-api.service';
-import { RentedBook } from '../RentedBook';
+
 import * as moment from 'moment';
 
 
@@ -14,7 +14,7 @@ export class RentedBookPageComponent implements OnInit {
   
   public cancelRental : boolean = false;
   public listOfData : RentedBooks[];
-  public rentedBook : RentedBook;
+
   public fromDate : string;
   public toDate : string;
   constructor(private rentedBooksService: RentedBookApiService) { }
@@ -23,28 +23,26 @@ export class RentedBookPageComponent implements OnInit {
     this.rentedBooksService.getRentedBooks().subscribe(data =>{ 
       this.listOfData = data;
       console.log(this.listOfData);
-      console.log("test " + this.rentedBook.id);
-      this.fromDate = moment(this.listOfData[0].fromDate).startOf('hour').fromNow();
-      this.toDate = moment(this.listOfData[0].toDate).startOf('hour').fromNow();
+      for (let list of this.listOfData) {
+        this.fromDate = moment(list.fromDate).format('MMMM Do YYYY, HH:mm:ss');
+        this.toDate = moment(list.toDate).format('MMMM Do YYYY, HH:mm:ss');
+      }
       
     });
-    //TODO : unrent book
+    
 
     
   }
   
   
-
   unRentBook(id:number){
     this.rentedBooksService.unRentBook(id).subscribe(data=> {
       if (data == true) {
         this.cancelRental = true;
-        console.log("cancel rental" + this.cancelRental + this.rentedBook.id)
-      } else {
-        window.alert("Can't cancel rental");
         
+      } else {
+        window.alert("Can't cancel rental"); 
     }})
-    console.log("cancel rental" + this.cancelRental)
   }
 
 }
