9308ed327a51c4df77bf6e95153f2ea150c00e26
ignatandrei <ignatandrei@yahoo.com>
2020-02-29 07:45:12 +0200

    *ngIf="!data.cancelRental" and unRentBook(id: number)

--- src/WebDashboardNG/book-dash/src/app/rented-book-page/rented-book-page.component.ts
+++ src/WebDashboardNG/book-dash/src/app/rented-book-page/rented-book-page.component.ts
@@ -11,38 +11,45 @@ import * as moment from 'moment';
   styleUrls: ['./rented-book-page.component.css']
 })
 export class RentedBookPageComponent implements OnInit {
-  
-  public cancelRental : boolean = false;
-  public listOfData : RentedBooks[];
 
-  public fromDate : string;
-  public toDate : string;
+  public cancelRental: boolean = false;
+  public listOfData: RentedBooks[];
+
+  public fromDate: string;
+  public toDate: string;
   constructor(private rentedBooksService: RentedBookApiService) { }
 
   ngOnInit() {
-    this.rentedBooksService.getRentedBooks().subscribe(data =>{ 
+    this.rentedBooksService.getRentedBooks().subscribe(data => {
       this.listOfData = data;
       console.log(this.listOfData);
-      for (let list of this.listOfData) {
+      for (const list of this.listOfData) {
         this.fromDate = moment(list.fromDate).format('MMMM Do YYYY, HH:mm:ss');
         this.toDate = moment(list.toDate).format('MMMM Do YYYY, HH:mm:ss');
+        list.cancelRental = false;
       }
-      
-    });
-    
 
-    
+    });
+    it;
+
+
   }
-  
-  
-  unRentBook(id:number){
-    this.rentedBooksService.unRentBook(id).subscribe(data=> {
+
+
+  unRentBook(id: number) {
+    this.rentedBooksService.unRentBook(id).subscribe(data => {
       if (data == true) {
-        this.cancelRental = true;
-        
+        // this se refera la acest component care are TOT array de carti
+        // this.cancelRental = true;
+        // eu vreau pe book
+        const findIDArray =this.listOfData.filter(it => it.book.id === id);
+        console.log(`found number book array rented with id ${id} : ${findIDArray.length}`);
+        //should be one - we discuss if the argument in id or bookrented
+        findIDArray[0].cancelRental = true;
+
       } else {
-        window.alert("Can't cancel rental"); 
-    }})
+        window.alert('Can\'t cancel rental');
+    }});
   }
 
 }
